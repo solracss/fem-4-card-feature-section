bece481de92517c637eb7a65c75cd44d470973a5
solracss <herekespanol@gmail.com>
2023-08-12 22:05:12 +0200

    Update repo name

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'four-card-feature-section',
+	base: "fem-4-card-feature-section",
 });
